2330b0195e2def8206de3f81fa50f6b4451a901e
enescidem <enescidem1@hotmail.com>
2025-09-23 16:26:27 +0300

    Use Scheduled

---
+++ exception-management/src/main/java/com/enescidem/scheduled/ScheduledExample.java
@@ -0,0 +1,18 @@
+package com.enescidem.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//ilk olarak starterda aktif hale getirmek gerekir @EnableScheduling
+
+@Component //spring conteynırda bean oluşturmaya yarayan anatasyon
+public class ScheduledExample {
+	
+	// * * * * * *
+	@Scheduled(cron = "*/5 * * * * *")
+	public void write1To10() {
+		for(int i=1; i<=10; i++) {
+			System.out.print(i+" ");
+		}
+	}
+}
--- exception-management/src/main/java/com/enescidem/starter/ExceptionManagementStarter.java
+++ exception-management/src/main/java/com/enescidem/starter/ExceptionManagementStarter.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.enescidem"})//entityleri scan edip başlatabilmesi için
 @EnableJpaRepositories(basePackages = {"com.enescidem"})//repository enable ediyoruz burada da
 @ComponentScan(basePackages = {"com.enescidem"})//componentleri de bunla enable ediyoruz
 @SpringBootApplication
+@EnableScheduling
 public class ExceptionManagementStarter {
 
 	public static void main(String[] args) {
